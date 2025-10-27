38fd1185606015963b6380d0539ed664adcc324e
abenezer121 <webgoat12@gmail.com>
2025-10-27 21:22:55 +0300

    first commit

---
+++ app/models/task.rb
@@ -0,0 +1,16 @@
+class Task < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :due_date, presence: true
+  validates :status, presence: true
+  validates :priority, presence: true
+
+  enum :status, {
+    pending: 0,
+    in_progress: 1,
+    completed: 2
+  }
+
+end
